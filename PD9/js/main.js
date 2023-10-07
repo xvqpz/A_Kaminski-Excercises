437f948473a49a4cb1741766dd580b77e3c3a4cd
xvqpz <adomaskaminski@gmail.com>
2023-10-07 15:17:40 +0300

    9 uzd

---
+++ PD9/js/main.js
@@ -0,0 +1,11 @@
+let numberX = 10;
+let numberY = '20';
+console.log(numberX == numberY); // false
+console.log(numberX === numberY); // false
+console.log(numberX != numberY); // true
+console.log(numberX !== numberY); // true
+console.log(numberX > numberY); // false
+console.log(numberX >= numberY); // false
+console.log(numberX < numberY); // true
+console.log(numberX <= numberY); // true
+console.log((numberX != numberY)?'It is true':'It is false'); // It is true
