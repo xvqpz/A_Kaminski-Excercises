9894977cee9bfc59390310de30dc57703460abee
xvqpz <adomaskaminski@gmail.com>
2023-10-07 15:08:48 +0300

    8 uzd

---
+++ PD8/js/main.js
@@ -0,0 +1,20 @@
+let numberX = 10;
+let numberY = 20;
+let result = numberX + numberY;
+console.log(result); // 30
+result += 10;
+console.log(result); // 40
+result -= 10;
+console.log(result); // 30
+result *= 10;
+console.log(result); // 300
+result /= 10;
+console.log(result); // 30
+result %= 10;
+console.log(result); // 0
+result **= 10;
+console.log(result); // 0
+result += 10 + numberY;
+console.log(result); // 30
+result *= numberX - numberY;
+console.log(result); // -300
