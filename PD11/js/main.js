ad045506636afea5552fe04ccce0e9520168d743
xvqpz <adomaskaminski@gmail.com>
2023-10-07 15:53:39 +0300

    11 uzd

---
+++ PD11/js/main.js
@@ -0,0 +1,17 @@
+let bitNumberOne = 10;
+let bitNumberTwo = 20;
+console.log(bitNumberOne & bitNumberTwo); // 0
+console.log(bitNumberOne | bitNumberTwo); // 30
+console.log(~bitNumberTwo); // -21
+console.log(bitNumberOne ^ bitNumberTwo); // 30
+console.log(bitNumberOne << 1); // 20
+console.log(bitNumberOne << 5); // 320
+console.log(bitNumberOne >> 1); // 5
+console.log(bitNumberOne >>> 1); // 5
+console.log((-100 >>> 3).toString(2)); // 11111111111111111111111110011
+console.log(true << 1); // 2
+console.log(false << 1); // 0
+console.log(true >> 1); // 0
+console.log(false >> 1); // 0
+console.log(~false); // -1
+console.log(~true); // -2
