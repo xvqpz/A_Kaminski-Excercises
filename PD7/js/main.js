367f68011885bb5a493d27b491642db5337436a0
xvqpz <adomaskaminski@gmail.com>
2023-10-07 14:56:46 +0300

    7 uzduotis

---
+++ PD7/js/main.js
@@ -0,0 +1,16 @@
+let numberOne = 10;
+let numberTwo = 20;
+console.log(numberOne + numberTwo); // 30
+console.log(numberOne - numberTwo); // -10
+console.log(numberOne * numberTwo); // 200
+console.log(numberOne / numberTwo); // 0.5
+console.log(numberOne % numberTwo); // 10
+console.log(numberOne ** numberTwo); // 100000000000000000000
+console.log(numberOne++); // 10
+console.log(++numberOne); // 12
+console.log(numberOne--); // 12
+console.log(--numberOne); // 10
+console.log(--numberOne + numberTwo++); // 29
+console.log(numberOne-- - --numberTwo + numberTwo++); // 9
+console.log(numberOne++ / numberTwo++); // 0.38095238095238093
+console.log(numberOne++ * --numberTwo); // 189
