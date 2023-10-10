912901103cb3456df3359fb94960bee5d483c498
xvqpz <adomaskaminski@gmail.com>
2023-10-10 10:20:05 +0300

    fixed

--- PD5/js/main.js
+++ PD5/js/main.js
@@ -12,5 +12,38 @@ console.log(typeof bigIntType);
 console.log(typeof stringType);
 console.log(typeof booleanType);
 console.log(typeof undefinedType);
-console.log(nullType);
+console.log(typeof nullType);
 console.log(typeof objectType);
+
+let number = 12;
+let otherNumber = 45.89;
+let textOne = 'Beautiful JavaScript World';
+let textTwo = "Beautiful JavaScript World";
+let textThree = `Beautiful JavaScript World`;
+let isTrue = (true == false);
+let isEqual = (2 >= 0);
+let petName = undefined;
+let petAge = null;
+let human = {
+humanName: undefined,
+humanAge: null
+};
+
+console.log(typeof number);
+console.log(typeof otherNumber);
+console.log(typeof textOne);
+console.log(typeof textTwo);
+console.log(typeof textThree);
+console.log(typeof isTrue);
+console.log(typeof isEqual);
+console.log(typeof petName);
+console.log(typeof petAge);
+console.log(typeof human);
+console.log(typeof humanName);
+console.log(typeof humanAge);
+
+
+
+
+
+
