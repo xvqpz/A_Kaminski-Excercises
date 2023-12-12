ae1dc982a3eaeeb03a174de240f376eeb8b1a863
yuusticia <yuusticia@gmail.com>
2023-12-12 13:24:06 +0200

    boba

---
+++ PD16/8/js/main.js
@@ -0,0 +1,24 @@
+//Parašykite JavaScript funkciją, kuri generuoja peteikto žodžio visus įmanomus raidinius
+//derinius (pvz.: cat –> c, a, ca, t, ct, cat, act, cta, tca, ...).
+
+
+// let arr = '123';
+
+// function scrambler(array) {
+//     let arr = [...array];
+
+//     if(let i = 0; i < arr.length){
+//         for(i < arr.length; i++){
+
+//         }
+//     }
+
+
+// };
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[(Math.floor(Math.random() * arr.length))]);
+
+
+
+// scrambler(arr);
